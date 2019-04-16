9fa433fccd5c64b99afd9a467d5a1d08eb8da457
itaxtl <itaxtl@gmail.com>
2019-04-16 13:48:05 +0200

    primer commit

---
+++ js/js2_2.js
@@ -0,0 +1,10 @@
+const FACTOR = 1.12986;
+var euro = prompt ("Cantidad en euros: ");
+
+console.log(euro + " euros son " + (euro * FACTOR) + "dolares");
+document.write(euro  + " euros son " + (euro * FACTOR) + " dólares.");
+
+
+
+
+
